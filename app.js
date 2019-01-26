59cfa1fe8859b81c8890a7ed8b4f043a673d6b8e
williamfiallos <wfiallos10@gmail.com>
2019-01-26 15:26:06 -0500

    update class exercise of express-post

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app     = express();
+const hbs     = require('hbs');
+const bodyParser = require('body-parser');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// route
+
+app.get('/', (req, res) => {
+  res.render('user-info-form');
+});
+
+
+app.get('/display-user-info', (req, res, next) => {
+  // let name = req.query.name;
+  // let age = req.query.age;
+  // let superhero = req.query.superhero;
+  // in ES6 Syntax:
+  // however, switch let to const since it is to never change in the real world:
+  const { name, age, superhero } = req.query;
+
+  res.send(`
+    Your name is ${name}
+    Your age is ${age}
+    Your favorite superhero is ${superhero}
+    `)
+});
+
+app.get('/login', (req, res) => {
+  res.render('login')
+});
+
+// app.post('/login', (req, res) => {
+//   res.send('You\'ve logged in!');
+// }); go to the following update below
+
+// app.post('/login', (req, res) => {
+//   res.send(req.body);
+// }); go to the following update below
+
+app.post('/login', (req, res) => {
+  // let email    = req.body.email;
+  // let password = req.body.password;
+  
+  // in ES6 syntax:
+  // let {email, password} = req.body
+  // however, switch let to const since it is to never change in the real world:
+  const { email, password } = req.body
+  
+  if ( email === "ironhacker@gmail.com" && password === "password"){
+    res.send ("Welcome")
+  } else {
+    res.send ("Go Away")
+  }
+
+  // res.send(`Email: ${email}, Password: ${password}`);
+});
+
+
+app.listen(3000, () => console.log('App listening on port 3000!'))
